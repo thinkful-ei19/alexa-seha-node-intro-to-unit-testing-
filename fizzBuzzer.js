2edff797f7de5596ea0223b8a4529bb3baf05372
Alexa Scott <arscott4792@gmail.com>
2018-03-09 09:09:24 -0800

    completed fizz-buzzer test

--- fizzBuzzer.js
+++ fizzBuzzer.js
@@ -1,17 +1,21 @@
-module.exports = function(num) {
-	if (typeof num !== 'number') {
-		throw Error('`num` must be a number');
-	}
-	if (num % 15 === 0) {
-		return 'fizz-buzz';
-	}
-	if (num % 5 === 0) {
-		return 'buzz';
-	}
-	if (num % 3 === 0) {
-		return 'fizz';
-	}
-	else {
-		return num;
-	}
+'use strict';
+
+function buzzer(num) {
+  if (typeof num !== 'number') {
+    throw Error('`num` must be a number');
+  }
+  if (num % 15 === 0) {
+    return 'fizz-buzz';
+  }
+  if (num % 5 === 0) {
+    return 'buzz';
+  }
+  if (num % 3 === 0) {
+    return 'fizz';
+  }
+  else {
+    return num;
+  }
 }
+
+module.exports = buzzer;
---
+++ test/test-fizz-buzzer.js
@@ -0,0 +1,59 @@
+'use strict';
+
+// import chai, declare expect variable
+const expect = require('chai').expect;
+
+// import buzzer
+const buzzer = require('../fizzBuzzer');
+
+// unit tests for our `buzzer` function
+describe('buzzer', function() {
+  it('number is divisible by 15', function() {
+    const normalCases = [
+      {num: 45, expected: 'fizz-buzz'},
+      {num: 60, expected: 'fizz-buzz'},
+    ];
+    normalCases.forEach(function(input) {
+      const answer = buzzer(input.num);
+      expect(answer).to.equal(input.expected);
+    });
+  });
+
+  it('number is divisible by 5', function() {
+    const normalCases = [
+      {num: 20, expected: 'buzz'},
+      {num: 25, expected: 'buzz'},
+    ];
+    normalCases.forEach(function(input) {
+      const answer = buzzer(input.num);
+      expect(answer).to.equal(input.expected);
+    });
+  });
+
+  it('number is divisible by 3', function() {
+    const normalCases = [
+      {num: 12, expected: 'fizz'},
+      {num: 24, expected: 'fizz'},
+    ];
+    normalCases.forEach(function(input) {
+      const answer = buzzer(input.num);
+      expect(answer).to.equal(input.expected);
+    });
+  });
+
+  it('if args are not numbers', function() {
+    const badInputs = [
+      ['num'],
+      ['1'],
+      [false]
+    ];
+    badInputs.forEach(function(input) {
+      expect(function() {
+        buzzer(input);
+      }).to.throw(Error);
+    });
+  });
+
+
+
+});
